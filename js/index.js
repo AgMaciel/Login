928d81cab9690d8f18cea85e373bc4d1f8d13b06
AGIS GOMES MACIEL <agismaciel@hotmail.com>
2023-11-03 07:38:55 -0300

    atualizando o projeto de Login de Loja
    Atualizando o projeto LOGIN, incluindo as telas de login, registro e esqueceu a senha

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
-function botao(){
-
-}
+document.getElementById("btn_Registrar").addEventListener("click", function() {
+    // Adiciona uma classe para aplicar o efeito
+    this.classList.add("highlight");
+    
+    // Remove a classe após um curto atraso (1 segundo no exemplo)
+    setTimeout(() => {
+      this.classList.remove("highlight");
+    }, 1000); // 1000 milissegundos (1 segundo)
+  });
+  
+  let currentIndex = 0;
+  const slides = document.querySelectorAll(".slide");
+  
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+  }
+  
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  }
+  
+  setInterval(nextSlide, 5000); // Troca os slides automaticamente a cada 5 segundos
+  showSlide(currentIndex);
+  
